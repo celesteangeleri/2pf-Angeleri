1b5c7eb8164af1aefe2c5337d5b3c860d249192d
Celeste Angeleri <angelericeleste@gmail.com>
2022-05-03 22:39:51 -0300

    guard funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,36 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () =>
       import('./feature/components/home/home.module').then((m) => m.HomeModule),
-       canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
-
+  {
+    path: 'alumnos',
+    loadChildren: () =>
+      import('./feature/components/student-abm/student-abm.module').then(
+        (m) => m.StudentAbmModule
+      ),canActivate: [AuthGuard],
+  },
+  {
+    path:'cursos',
+    loadChildren: () =>
+      import('./feature/components/cursos/cursos.module').then(
+        (m) => m.CursosModule
+      ),canActivate: [AuthGuard],
+  },
+  {
+    path:'inscripcione',
+    loadChildren: () =>
+      import('./feature/components/student-form/student-form.module').then(
+        (m) => m.StudentFormModule
+      ),canActivate: [AuthGuard],
+  },
   {
     path: 'autenticacion',
     loadChildren: () =>
       import('./feature/components/autenticacion/autenticacion.module').then(
         (m) => m.AutenticacionModule
-      ),  
- 
+      ),
+    // canActivate: [AuthGuard],
   },
 ];
 
