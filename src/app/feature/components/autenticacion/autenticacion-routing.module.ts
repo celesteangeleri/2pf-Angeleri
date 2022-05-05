fb4c27197aece43a97ed1c3cc8826c785dbc1904
Celeste Angeleri <angelericeleste@gmail.com>
2022-05-05 16:10:43 -0300

    test de componente login

--- src/app/feature/components/autenticacion/autenticacion-routing.module.ts
+++ src/app/feature/components/autenticacion/autenticacion-routing.module.ts
@@ -15,38 +15,31 @@ const routes: Routes = [
       {
         path: 'registro',
         component: RegistroComponent,
-        canActivateChild: [AuthGuard],
       },
       {
         path: 'login',
         component: LoginComponent,
-        canActivateChild: [AuthGuard],
       },
       {
         path: 'cursos',
         component: CursosComponent,
-        canActivateChild: [AuthGuard],
       },
       {
         path: 'cursos/:id',
         component: CursosComponent,
-        canActivateChild: [AuthGuard],
       },
       {
         path: 'inicio',
         component: HomeComponent,
-        canActivateChild: [AuthGuard],
       },
-      { path: 'home', component: HomeComponent, canActivateChild: [AuthGuard] },
+      { path: 'home', component: HomeComponent },
       {
         path: 'inscripcione',
         component: StudentFormComponent,
-        canActivateChild: [AuthGuard],
       },
       {
         path: 'alumnos',
         component: StudentAbmComponent,
-        canActivateChild: [AuthGuard],
       },
     ],
   },
